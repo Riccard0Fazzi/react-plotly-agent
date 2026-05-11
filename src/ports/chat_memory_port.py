d3b157f5afcef062c68b216de979545926a52442
Riccardo Fazzi <riccardofazzi3@gmail.com>
2026-05-11 02:01:29 +0200

    fixed a abstractmethod missing

--- src/ports/chat_memory_port.py
+++ src/ports/chat_memory_port.py
@@ -2,11 +2,13 @@
 
 import abc
 
-class ChatMemoryPort:
+class ChatMemoryPort(abc.ABC):
     
+    @abc.abstractmethod
     def create_conversation(self, conversation_id: str) -> None:
         raise NotImplementedError
 
+    @abc.abstractmethod
     def save_message(
             self,
             conversation_id: str,
@@ -15,7 +17,8 @@ class ChatMemoryPort:
             metadata: dict | None = None,
     ) -> None:
         raise NotImplementedError
-    
+
+    @abc.abstractmethod
     def load_messages(self, conversation_id: str) -> list[dict[str,str]]:
         raise NotImplementedError
     
